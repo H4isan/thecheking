1657526d14e9dd0bdb7ec375ec6b7e921282840d
Andres Aristizabal <andresaristizabal@seven4n.com>
2017-05-06 09:29:32 -0500

    menu b1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,23 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("content-main").style.marginLeft = "0";
 }
+(function(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function(){
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
+    }
+})
