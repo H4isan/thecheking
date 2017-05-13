48a88dc0cdb3105d2ee3c0a7352a7f3c2e285e2b
harlen <harlengiraldo@gmail.com>
2017-05-13 11:08:42 -0500

    style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,12 +21,14 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("content-main").style.marginLeft = "250px";
+    document.getElementById("overlay").style.width = "100%";
+    //document.getElementById("content-main").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("overlay").style.width = "0";
     document.getElementById("content-main").style.marginLeft = "0";
 }
 function findMoney(id){
